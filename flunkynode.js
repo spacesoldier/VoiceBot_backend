304344c890b1ce514ebdb4638decad6e81985833
spacesoldier <kibersoldier@gmail.com>
2016-04-23 21:17:02 +0300

    initial node logic - view raw form data

---
+++ flunkynode.js
@@ -0,0 +1,32 @@
+// curl -k https://localhost:8000/
+const https = require('https');
+const fs = require('fs');
+const urllib = require('url');
+
+const options = {
+  key: fs.readFileSync('test/fixtures/keys/agent2-key.pem'),
+  cert: fs.readFileSync('test/fixtures/keys/agent2-cert.pem')
+};
+
+/*
+{
+  href: '/status?name=ryan',
+  search: '?name=ryan',
+  query: 'name=ryan',
+  pathname: '/status'
+}
+*/
+
+
+https.createServer(options, (req, res) => {
+	var data = "";
+	req.on("data",chunk=>{data+=chunk});
+	req.on("end",()=>{
+		var currentURL = urllib.parse(req.url);
+		switch (currentURL.pathname){
+			case "/upload_images": fs.writeFileSync("123445",data); break;
+		}
+		res.writeHead(200);
+		res.end('\n');
+	});
+}).listen(8000);
